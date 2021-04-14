9bdd06581a9d3c444664087820257582b0e10edf
Jayaraj Vemula <jayaraj.vemula@gmail.com>
2021-04-14 11:23:17 +0530

    New Branch and adding new Code Files
    New Branch and adding new Code Files . Wil be merged back to master once Approved.

---
+++ AreaofRectangle_powershell.ps1
@@ -0,0 +1,26 @@
+﻿#sample program to caluclate Area of Rectangle in Powershell. 
+#version -1.0
+# Author : Jayaraj Vemula
+
+function Get-AreaofRectangle
+{
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]   
+         [ValidateRange(0,10000)]
+         [int] $Len,
+         [Parameter(Mandatory=$true, Position=1)]        
+         [ValidateRange(0,10000)]
+         [int] $Wid
+    )
+
+    
+    Write-Host "Area of Rectangle is : "  ($Len * $Wid)
+
+
+}
+
+Get-AreaofRectangle 15 10000
+#Testcase-1: Valid Range
+Get-AreaofRectangle 15 1000000
+#testCase-2 : Invalid Range
