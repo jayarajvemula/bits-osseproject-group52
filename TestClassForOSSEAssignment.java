de7b14b2349401bb4f16b2dd5412ba05bf65c1fd
Vellaisamy86 <66508392+Vellaisamy86@users.noreply.github.com>
2021-04-15 18:28:01 +0800

    New Java Class

---
+++ TestClassForOSSEAssignment.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author vellaisamysudalaimuthu
+ *
+ */
+public class TestClassForOSSEAssignment {
+	
+	private void sumOfTwoInt(int a, int b) {
+		System.out.println("Sum of given integers is: "+ (a+b));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClassForOSSEAssignment osse=new TestClassForOSSEAssignment();
+		osse.sumOfTwoInt(10, 20);
+
+	}
+
+}
