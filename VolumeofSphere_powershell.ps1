8f9a5da4950a4540d1ee6ee2ba40e3749a4fb92a
Jayaraj Vemula <jayaraj.vemula@gmail.com>
2021-04-15 14:41:28 +0530

    new script

---
+++ VolumeofSphere_powershell.ps1
@@ -0,0 +1,25 @@
+﻿#sample program to caluclate Area of Rectangle in Powershell. 
+#version -1.0
+# Author : Jayaraj Vemula
+
+function Get-VolumeofSphere
+{
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]   
+         [ValidateRange(0,10000)]
+         [int] $rad
+         
+
+    )
+
+    
+    Write-Host "Volume of Sphere is : "  (4/3)*([math]::pi)*[Math]::Pow($Rad,3)
+
+
+}
+
+Get-VolumeofSphere 15 
+#Testcase-1: Valid Range
+Get-VolumeofSphere 1000000
+#testCase-2 : Invalid Range
